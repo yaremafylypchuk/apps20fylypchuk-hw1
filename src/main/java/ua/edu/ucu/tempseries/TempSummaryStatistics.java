fb7445201140a69bf5c322157baff21f5caa8dad
yaremafylypchuk <57004357+yaremafylypchuk@users.noreply.github.com>
2020-11-13 16:12:07 +0200

    New commit

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,5 +1,11 @@
 package ua.edu.ucu.tempseries;
 
 public class TempSummaryStatistics {
-    
+    double avgTemp, devTemp, minTemp, maxTemp;
+    TempSummaryStatistics(double avgTemp, double devTemp, double minTemp, double maxTemp){
+        this.avgTemp = avgTemp;
+        this.devTemp = devTemp;
+        this.minTemp = minTemp;
+        this.maxTemp = maxTemp;
+    }
 }
--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -1,52 +1,137 @@
 package ua.edu.ucu.tempseries;
 
+import java.util.InputMismatchException;
+
 public class TemperatureSeriesAnalysis {
-
+    static int Mintemp = -273;
+    double [] temperaturesArray;
+    int currentTempnum;
+    int currentCapacity;
     public TemperatureSeriesAnalysis() {
-
+        temperaturesArray = new double[1];
+        currentCapacity = 1;
+        currentTempnum = 0;
     }
 
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
-
+        this();
+        addTemps(temperatureSeries);
+//        System.out.println(currentTempnum);
+//        System.out.println(currentCapacity);
     }
 
     public double average() {
-        return -1;
+        checkInput();
+        double avarage = 0;
+        for (int i = 0; i<currentTempnum; i++){
+            avarage += temperaturesArray[i];
+        }
+        avarage = avarage/currentTempnum;
+        return avarage;
     }
 
     public double deviation() {
-        return 0;
+        checkInput();
+        double deviation = 0;
+        double average = average();
+        for (int i = 0; i < currentTempnum; i++){
+            deviation += Math.pow(Math.abs(temperaturesArray[i] - average), 2);
+        }
+
+        deviation /= currentTempnum;
+        return deviation;
     }
 
     public double min() {
-        return 0;
+        checkInput();
+        double min = temperaturesArray[0];
+        for (int i = 0; i < currentTempnum; i ++){
+            if (min > temperaturesArray[i]){
+                min = temperaturesArray[i];
+            }
+        }
+        return min;
     }
 
     public double max() {
-        return 0;
+        checkInput();
+        double max = temperaturesArray[0];
+        for (int i = 0; i < currentTempnum; i ++){
+            if (max < temperaturesArray[i]){
+                max = temperaturesArray[i];
+            }
+        }
+        return max;
     }
 
     public double findTempClosestToZero() {
-        return 0;
+        return findTempClosestToValue(0);
     }
 
     public double findTempClosestToValue(double tempValue) {
-        return 0;
+        checkInput();
+        double curClosest = temperaturesArray[0];
+        double distance = Math.abs(curClosest - tempValue);
+        for (int i = 0; i < currentTempnum; i ++){
+            if (distance > Math.abs(temperaturesArray[i] - tempValue)){
+                curClosest = temperaturesArray[i];
+                distance = Math.abs(curClosest - tempValue);
+            }
+        }
+        return curClosest;
     }
 
     public double[] findTempsLessThen(double tempValue) {
-        return null;
+        TemperatureSeriesAnalysis res = new TemperatureSeriesAnalysis();
+        for (int i =0; i < currentTempnum; i++){
+            if (temperaturesArray[i] < tempValue){
+                res.addTemps(temperaturesArray[i]);
+            }
+        }
+        double [] result = new double[res.currentTempnum];
+        System.arraycopy(res.temperaturesArray, 0, result, 0, res.currentTempnum);
+        return result;
     }
 
     public double[] findTempsGreaterThen(double tempValue) {
-        return null;
+        TemperatureSeriesAnalysis res = new TemperatureSeriesAnalysis();
+        for (int i =0; i < currentTempnum; i++){
+            if (temperaturesArray[i] > tempValue){
+                res.addTemps(temperaturesArray[i]);
+            }
+        }
+        double [] result = new double[res.currentTempnum];
+        System.arraycopy(res.temperaturesArray, 0, result, 0, res.currentTempnum);
+        return result;
     }
 
     public TempSummaryStatistics summaryStatistics() {
-        return null;
+        checkInput();
+        return new TempSummaryStatistics(average(), deviation(), min(), max());
+    }
+    public void checkInput(){
+        if (currentTempnum == 0){
+            throw new InputMismatchException();
+        }
     }
 
     public int addTemps(double... temps) {
-        return 0;
+        for (double temp: temps){
+            if (temp < Mintemp){
+                throw new InputMismatchException();
+            }
+            if (currentTempnum < currentCapacity){
+                temperaturesArray[currentTempnum] = temp;
+                currentTempnum ++;}
+            else{
+                double[] newArray = new double[currentCapacity * 2];
+                System.arraycopy(temperaturesArray, 0, newArray, 0, currentCapacity);
+                currentCapacity*=2;
+                temperaturesArray = newArray;
+                temperaturesArray[currentTempnum] = temp;
+                currentTempnum ++;
+            }
+        }
+        return currentTempnum;
     }
 }
